639d4738daf5e75ce3e2e4f8c9e6097ac4b2f29c
Jake J <jakejohnson818@gmail.com>
2018-04-30 17:00:36 -0500

    Finished the squared exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,22 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        int i2 = 100;
+//        do {
+//            if (i2 % 5 == 0){
+//                System.out.println(i2);}
+//            i2--;
+//        } while (i2 >= -10);
 
-        int i2 = 0;
+        long i3 = 2;
         do {
-            if (i2 % 2 == 0){
-                System.out.println(i2);}
-            i2++;
-        } while (i2 <= 100);
+            System.out.println(i3);
+            i3*=i3;
+        } while (i3 <= 65536);
     }
 }
