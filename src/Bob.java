92fe7f6d02e9175db6e9370c29d25e1a6db52c49
Jake J <jakejohnson818@gmail.com>
2018-05-01 11:02:26 -0500

    Made Bob the teenager

--- src/Bob.java
+++ src/Bob.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scResponse = new Scanner(System.in);
+        System.out.println("Do you want to talk to Bob? Yes or No");
+        String userResponse = scResponse.nextLine();
+        if (userResponse.equalsIgnoreCase("yes")) {
+            System.out.println("Exit at anytime by typing \"x\"");
+            System.out.println("Talk to Bob: ");
+            while (true) {
+                Scanner scUserInput = new Scanner(System.in);
+                String userInput = scUserInput.nextLine();
+                if (userInput.endsWith("!")) {
+                    System.out.println("Whoa, chill out!");
+                } else if (userInput.endsWith("?")) {
+                    System.out.println("Sure.");
+                } else if (userInput.endsWith(".")) {
+                    System.out.println("Whatever.");
+                } else if (userInput.endsWith("x")) {
+                    System.out.println("Fine. Be that way!");
+                    break;
+                } else {
+                    System.out.println("What was that?");
+                }
+            }
+        }
+    }
 }
